f38f25558be00816b29d6a6682389068fb4d93f5
LuiiVuitton <Luis1134@icloud.com>
2023-11-13 20:02:59 -0500

    Created the Login Screen Demo

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,68 @@
-import { View, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text, Image } from "react-native";
+import { TextInput } from "react-native";
+import { TouchableOpacity } from "react-native";
 
 export default function LoginScreen() {
   return (
     <View className="bg-white h-full w-full">
-      <Text>LoginScreen</Text>
+      <StatusBar style="light" />
+
+      {/*Background*/}
+      <Image
+      //className="h-full w-full "
+      //source={require("../assets/C2A_LOGO.png")}
+      />
+
+      {/*logo*/}
+      <View className="flex-row justify-around w-full absolute">
+        <Image
+        //className="h-[300] w-[300]"
+        //source={require("../assets/C2A_LOGO.png")}
+        />
+      </View>
+
+      {/*Title and Form*/}
+
+      <View className="h-full w-full flex justify-around pt-40 pb-10">
+        {/*Title*/}
+        <View className="flex items-center">
+          <Text className="text-black  font-bold tracking-wider text-5xl">
+            HomeSalePlus
+          </Text>
+        </View>
+        {/*Form*/}
+        <View className="flex items-center mx-4 space-y-4">
+          <View className="bg-black/5 p-5 rounded-2xl w-full">
+            <TextInput placeholder="Email" placeholderTextColor={"gray"} />
+          </View>
+          <View className="bg-black/5 p-5 rounded-2xl w-full mb-3">
+            <TextInput
+              placeholder="Password"
+              placeholderTextColor={"gray"}
+              secureTextEntry
+            />
+          </View>
+
+          <View className="w-full">
+            <TouchableOpacity>
+              <View className="w-full bg-sky-400 p-3 rounded-2xl mb-3">
+                <Text className="text-xl font-bold text-white text-center">
+                  Login
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <View className="flex-row justify-center">
+            <Text>Don't have an account?</Text>
+            <TouchableOpacity>
+              <Text className="text-sky-600">SignUp</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
